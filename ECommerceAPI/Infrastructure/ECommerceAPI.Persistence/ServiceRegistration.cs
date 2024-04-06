3d48a5d674d431b2432393de9eb10cf323759e02
Suraj210 <surajji@code.edu.az>
2024-04-06 13:52:55 +0200

    Create Database

--- ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -1,5 +1,5 @@
-﻿using ECommerceAPI.Application.Abstractions;
-using ECommerceAPI.Persistence.Concretes;
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ECommerceAPI.Persistence
@@ -8,7 +8,8 @@ namespace ECommerceAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ECommerceAPIDbContext>(options=>options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ECommerceAPIDb;"));
+
         }
     }
 }
