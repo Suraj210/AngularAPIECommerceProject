7b042ccbee9ec45bcad762995890735932d909d3
Suraj210 <surajji@code.edu.az>
2024-04-29 21:12:50 +0200

    Product add in admin panel

---
+++ ECommerceClient/src/app/admin/components/products/create/create.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../../../services/common/models/product.service';
+import { Create_Product } from '../../../../contracts/create_product';
+import { BaseComponent, SpinnerType } from '../../../../base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../../../services/admin/alertify.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss',
+})
+export class CreateComponent extends BaseComponent {
+  constructor(
+    spinner: NgxSpinnerService,
+    private productService: ProductService,
+    private alertify: AlertifyService
+  ) {
+    super(spinner);
+  }
+
+  ngOnInit(): void {}
+
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
+    this.showSpinner(SpinnerType.BallAtom);
+    const product_create: Create_Product = new Create_Product();
+    product_create.name = name.value;
+    product_create.stock = parseInt(stock.value);
+    product_create.price = parseFloat(price.value);
+
+    this.productService.create(product_create, () => {
+      
+      this.hideSpinner(SpinnerType.BallAtom);
+      this.alertify.message('Product has been added successfully', {
+        dismissOthers: true,
+        messageType: MessageType.Success,
+        position: Position.TopRight,
+      });
+    });
+  }
+}
--- ECommerceClient/src/app/admin/components/products/products.component.ts
+++ ECommerceClient/src/app/admin/components/products/products.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BaseComponent, SpinnerType } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpClientService } from '../../../services/common/http-client.service';
-import { Product } from '../../../contracts/product';
 
 @Component({
   selector: 'app-products',
@@ -19,53 +18,5 @@ export class ProductsComponent extends BaseComponent {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
-
-    this.httpClientService
-      .get<Product[]>({
-        controller: 'products',
-      })
-      .subscribe((data) => {
-        console.log(data);
-      });
-
-    // this.httpClientService
-    //   .post(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     {
-    //       name: 'Kalem',
-    //       stock: 100,
-    //       price: 15,
-    //     }
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .put(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     {
-    //       id: 'a9ec79c6-4627-40c7-b2be-490c9a95697e',
-    //       name: 'Dry Paper',
-    //       stock: 111,
-    //       price: 5.5,
-    //     }
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .delete(
-    //     { controller: 'products' },
-    //     'a9ec79c6-4627-40c7-b2be-490c9a95697e'
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .get({
-    //     fullEndPoint: 'https://jsonplaceholder.typicode.com/posts',
-    //   })
-    //   .subscribe((data) => console.log(data));
   }
 }
