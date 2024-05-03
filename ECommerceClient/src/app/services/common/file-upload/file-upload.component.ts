1e2628f7420161049346261a824aef1ba6ce2278
Suraj210 <surajji@code.edu.az>
2024-05-03 16:24:54 +0200

    File Upload first part

---
+++ ECommerceClient/src/app/services/common/file-upload/file-upload.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NgxFileDropEntry } from 'ngx-file-drop';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrl: './file-upload.component.scss'
+})
+export class FileUploadComponent {
+  public files: NgxFileDropEntry[];
+
+  public selectedFiles(files: NgxFileDropEntry[]) {
+    this.files = files;
+    const fileData : FormData = new FormData();
+    for (const file of files) {
+      (file.fileEntry as FileSystemFileEntry).file((_file:File)=>{
+        fileData.append(_file.name,_file,file.relativePath)
+      })
+    }
+
+    
+  }
+
+
+}
